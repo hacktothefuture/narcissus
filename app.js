90aecb0b32aadcb857fb86bed1c05141f2f636ae
Connor McMahon <connorm333@gmail.com>
2015-09-19 15:11:32 -0400

    Added function to calculate score for a location given a post

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 function getOptimalTrip(userParams){
 	var locations = getLocationsByBudget(userParams);
 	var locationsWithScores = locations.map(calculateLocationScore);
-	return locationsWithScores.sorted();
+	return locationsWithScores.sort(function(a,b) { return a.score - b.score});
 }
 
 //Grab Locations from Amadeus API
@@ -13,5 +13,19 @@ function getLocationsByBudget(userParams){
 
 //Calculate instagram scores for each location
 function calculateLocationScore(location){
-	
+
+}
+
+//Given Instagram API data, calculate bonus
+function postsToScore(location, posts){
+	var sortedPosts = posts.sort(function(a,b) { return a.likes.count - b.likes.count; });
+	var postsWithoutOutliers = sortedPosts.splice(Math.floor(sortedPosts.length/10), 
+												  Math.ceil(sortedPosts.length*9/10));
+	var totalLikes = 0;
+	for(var i = 0; i < postsWithoutOutliers.length; i++){
+		totalLikes += postsWithoutOutliers[i].likes.count;
+	}
+	var normalizedLikes = totalLikes/postsWithoutOutliers.length;
+	location.score = normalizedLikes;
+	return location;
 }
