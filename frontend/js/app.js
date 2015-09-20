2b2eec795fc710302e9b330424b8f2797e58145c
Kris Lange <lange649@umn.edu>
2015-09-20 03:48:37 -0400

    init line drawing

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -14,7 +14,7 @@ function findTrips() {
     var budget = $("#budget").val();
 
     var response = $.get("http://localhost:2020/node/findtrip?departure_location=" + location + "&depart_date=" + departureDate + "&budget=" + budget, function(data) {
-        alert(data);
+        drawMapStuff(data);
     });
 }
 
@@ -23,7 +23,20 @@ function makeTable(data){
 }
 
 function drawMapStuff(data){
-
+    for(var i=0; i < data.length;i++){
+        alert(JSON.stringify(data[i]));
+        var coords = [{lat: 0, lng: -180}, {lat: data[i].lat, lng: data[i].lng}];
+        
+        var flightPath = new google.maps.Polyline({
+                                                  path: coords,
+                                                  geodesic: true,
+                                                  strokeColor: '#FF0000',
+                                                  strokeOpacity: 1.0,
+                                                  strokeWeight: 2
+                                                  });
+        
+        flightPath.setMap(map);
+    }
 }
 
 $( document ).ready(function() {
