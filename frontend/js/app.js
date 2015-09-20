b2b8a420d12949f08023e4ef64ef6407945d65eb
Mitchell Sternke <mitchellsternke@gmail.com>
2015-09-19 21:15:12 -0400

    Using jquery autocomplete for airports now

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,44 +1,18 @@
 var AMADEUS_KEY = "Q76ryFVSqb5BE6pmBJw9YJtuWsSufclH";
 
-var locationTypingTimer;
-
-function populateAirports( airports ) {
-    // Clear all current airports
-    $("#airport option").slice(1).remove();
-
-    for( var i = 0; i < airports.length; i++ ) {
-        var airport = airports[i];
-        $("#airport").append("<option value='" + airport.value + "'>" + airport.label + "</option>");
-    }
-}
-
-function findAirports() {
-    var search = $("#location").val();
-    
+function findAirports(search, response) {
     $.ajax({
         url: "http://api.sandbox.amadeus.com/v1.2/airports/autocomplete?apikey=" + AMADEUS_KEY +"&term=" + search,
     }).done(function(data) {
-        populateAirports(data);
+        response(data);
     });
-}
-
-function findTrips() {
-    $("#results-group").empty();
-    
-    // Query trips
-    var location = $("#location").val();
-    var departureDate = $("#departure-date").val();
-    var returnDate = $("#return-date").val();
-    var budget = $("#budget").val();
 }
 
 $( document ).ready(function() {
     // Bind event handlers
-    $("#btn-find-trips").click(function() { findTrips(); });
-    $("#location").keyup(function(){
-        clearTimeout(locationTypingTimer);
-        if( $("#location").val) {
-            locationTypingTimer = setTimeout(findAirports, 500);
+    $("#location").autocomplete({
+        source: function(request, response) {
+            findAirports(request.term, response);
         }
     });
 });
